8a055fae7b5c312ea3af71d82e299c2a03a1d820
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-03-27 10:37:45 -0300

    Add user model and authentication via devise-token-auth

--- wbooks-api/app/models/user.rb
+++ wbooks-api/app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
