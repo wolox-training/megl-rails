a6f060981a70083260f25e5c6b51d48c5050303a
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-03-28 13:20:41 -0300

    Fix tests of Book/Image

--- wbooks-api/app/models/image.rb
+++ wbooks-api/app/models/image.rb
@@ -1,3 +1,3 @@
 class Image < ApplicationRecord
-  has_one :book
+  has_one :book, dependent: :destroy
 end
--- wbooks-api/spec/models/book_spec.rb
+++ wbooks-api/spec/models/book_spec.rb
@@ -7,7 +7,7 @@ describe Book do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:genre) }
     it { is_expected.to validate_presence_of(:author) }
-    it { is_expected.to validate_presence_of(:image) }
+    it { is_expected.to belong_to(:image) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:publisher) }
     it { is_expected.to validate_presence_of(:year) }
