bca36e53e020d1229850c7c4be7eebef48040b96
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-03-28 17:53:22 -0300

    Fix nested module in BooksController

--- wbooks-api/app/controllers/api/v1/books_controller.rb
+++ wbooks-api/app/controllers/api/v1/books_controller.rb
@@ -1,15 +1,19 @@
-class Api::V1::BooksController < ApplicationController
-  include Wor::Paginate
+module Api
+  module V1
+    class BooksController < ApplicationController
+      include Wor::Paginate
 
-  before_action :authenticate_user!
+      before_action :authenticate_user!
 
-  def index
-    @books = Book.all
-    render_paginated @books, each_serializer: BookIndexSerializer
-  end
+      def index
+        @books = Book.all
+        render_paginated @books, each_serializer: BookIndexSerializer
+      end
 
-  def show
-    @book = Book.find(params[:id])
-    render json: @book, serializer: BookShowSerializer
+      def show
+        @book = Book.find(params[:id])
+        render json: @book, serializer: BookShowSerializer
+      end
+    end
   end
 end
