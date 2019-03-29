1cdcf9f5c48b2f220d189c3006fdf19f7e4fe1f5
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-03-29 13:29:53 -0300

    Fix user/rent factories

--- wbooks-api/spec/factories/rents.rb
+++ wbooks-api/spec/factories/rents.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :rent do
-    user { User.find_by(email: 'test@wolox.com.ar') }
+    user
     book
     rented_from { Faker::Date.backward(10) }
     rented_to { Faker::Date.forward(10) }
--- wbooks-api/spec/factories/users.rb
+++ wbooks-api/spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    email { 'test@wolox.com.ar' }
-    password { '123123123' }
-    password_confirmation { '123123123' }
-    first_name { 'Test' }
-    last_name { 'TestLastName' }
+    sequence(:email) { |n| "person#{n}@example.com" }
+    password { 'testtest' }
+    password_confirmation { 'testtest' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
