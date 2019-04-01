79c6839a8c97f3fcbdd78dd667cf07ff94f45819
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-04-01 17:30:41 -0300

    I18n of rent creation email

--- wbooks-api/app/mailers/user_mailer.rb
+++ wbooks-api/app/mailers/user_mailer.rb
@@ -3,6 +3,8 @@ class UserMailer < ApplicationMailer
     user = User.find(params[:user_id])
     @rent = Rent.find(params[:rent_id])
 
-    mail(to: user.email, subject: 'wbooks-api - Rent creation')
+    I18n.locale = user.locale || I18n.default_locale
+
+    mail(to: user.email, subject: t(:subject))
   end
 end
