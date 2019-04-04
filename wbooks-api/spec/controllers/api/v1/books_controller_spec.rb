b708b051b973f6be3f94447a1f6561b268e2bfaf
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-04-04 17:51:06 -0300

    Add tests for books#search

--- wbooks-api/spec/controllers/api/v1/books_controller_spec.rb
+++ wbooks-api/spec/controllers/api/v1/books_controller_spec.rb
@@ -58,4 +58,46 @@ describe Api::V1::BooksController do
       end
     end
   end
+
+  describe 'GET #search' do
+    book = {
+      isbn: '0385472579',
+      title: 'Zen speaks',
+      subtitle: 'shouts of nothingness',
+      number_of_pages: 159,
+      authors: [
+        'Zhizhong Cai'
+      ]
+    }
+
+    before do
+      open_library = class_double('OpenLibrary').as_stubbed_const
+      allow(open_library).to receive(:search).with('0385472579').and_return(book)
+      allow(open_library).to receive(:search).with('13483783354345434').and_return({})
+    end
+
+    context 'when searching a valid book' do
+      before do
+        get :search, params: { isbn: '0385472579' }
+      end
+
+      it 'responses with the book' do
+        expect(book.stringify_keys).to eq response_body
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching an invalid book' do
+      before do
+        get :search, params: { isbn: '13483783354345434' }
+      end
+
+      it 'responds with 404 status' do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+  end
 end
