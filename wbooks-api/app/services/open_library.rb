bdfd3b408cf9554ce9b69473fe73cb0ff7199a1e
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-04-08 17:40:12 -0300

    Refactor per PR requirement

--- wbooks-api/app/services/open_library.rb
+++ wbooks-api/app/services/open_library.rb
@@ -3,7 +3,17 @@ class OpenLibrary
     (isbn.length == 10 || isbn.length == 13) && isbn.scan(/\D/).empty?
   end
 
+  def self.build_response(response)
+    { isbn: response['identifiers']['isbn_10'].first,
+      title: response['title'],
+      subtitle: response['subtitle'],
+      number_of_pages: response['number_of_pages'],
+      authors: response['authors'].map { |a| a['name'] } }
+  end
+
   def self.search(isbn)
+    isbn.freeze
+
     return {} unless valid_isbn?(isbn)
 
     response = HTTParty.get("https://openlibrary.org/api/books?bibkeys=ISBN:#{isbn}"\
@@ -11,12 +21,6 @@ class OpenLibrary
 
     return {} if response.empty?
 
-    response = response[0]
-
-    { isbn: isbn,
-      title: response['title'],
-      subtitle: response['subtitle'],
-      number_of_pages: response['number_of_pages'],
-      authors: response['authors'].map { |a| a['name'] } }
+    response.empty? ? {} : build_response(response[0])
   end
 end
