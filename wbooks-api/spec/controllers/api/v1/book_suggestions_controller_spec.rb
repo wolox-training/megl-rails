fada773a13a1960d4920a6b7651413f5bdb17376
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-04-08 17:08:38 -0300

    Refactor tests per PR requirement

--- wbooks-api/spec/controllers/api/v1/book_suggestions_controller_spec.rb
+++ wbooks-api/spec/controllers/api/v1/book_suggestions_controller_spec.rb
@@ -6,37 +6,26 @@ describe Api::V1::BookSuggestionsController do
   include Response::JSONParser
 
   describe 'POST #create' do
+    subject(:http_request) { post :create, params: { book_suggestion: book_suggestion } }
+
     context 'When creating a valid book suggestion' do
       let!(:book_suggestion) { attributes_for(:book_suggestion) }
 
       it 'creates a new book suggestion' do
-        expect do
-          post :create, params: { book_suggestion: book_suggestion }
-        end.to change { BookSuggestion.count }.by(1)
+        expect { http_request }.to change { BookSuggestion.count }.by(1)
       end
 
-      it 'responds with 201 status' do
-        post :create, params: { book_suggestion: book_suggestion }
-        expect(response).to have_http_status(:created)
-      end
+      it { is_expected.to have_http_status(:created) }
     end
 
     context 'When creating an invalid book suggestion' do
       let!(:book_suggestion) { attributes_for(:book_suggestion, title: nil) }
 
-      before do
-        post :create, params: { book_suggestion: book_suggestion }
-      end
-
       it 'doesn\'t create a new book suggestion' do
-        expect do
-          post :create, params: { book_suggestion: book_suggestion }
-        end.to change { BookSuggestion.count }.by(0)
+        expect { http_request }.to change { BookSuggestion.count }.by(0)
       end
 
-      it 'responds with 400 status' do
-        expect(response).to have_http_status(:bad_request)
-      end
+      it { is_expected.to have_http_status(:bad_request) }
     end
   end
 end
