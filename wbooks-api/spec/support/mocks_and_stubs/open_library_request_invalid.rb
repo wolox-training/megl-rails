27b415ea4c64fc6712db4f76902aee81e6ea9cff
Marcelo Guzman <marcelo.guzman@wolox.com.ar>
2019-04-10 16:49:30 -0300

    Fix tests and use var env for openlibrary paths

--- wbooks-api/spec/support/mocks_and_stubs/open_library_request_invalid.rb
+++ wbooks-api/spec/support/mocks_and_stubs/open_library_request_invalid.rb
@@ -3,13 +3,14 @@ module OpenLibraryRequestInvalid
     WebMock.stub_request(:get, url)
            .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
            .to_return(
-             status: :ok,
+             status: 200,
              body: '{}',
              headers: { 'Content-Type': 'application/json' }
            )
   end
 
   def self.url
-    'https://openlibrary.org/api/books?bibkeys=ISBN:13483783354345434&format=json&jscmd=data'
+    "#{Rails.application.secrets.open_library_api}books?bibkeys=ISBN:13483783354345434" \
+    '&format=json&jscmd=data'
   end
 end
--- wbooks-api/spec/support/mocks_and_stubs/open_library_request_success.rb
+++ wbooks-api/spec/support/mocks_and_stubs/open_library_request_success.rb
@@ -3,13 +3,14 @@ module OpenLibraryRequestSuccess
     WebMock.stub_request(:get, url)
            .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
            .to_return(
-             status: :ok,
+             status: 200,
              body: File.read('./spec/support/fixtures/open_library_response_success.json'),
              headers: { 'Content-Type': 'application/json' }
            )
   end
 
   def self.url
-    'https://openlibrary.org/api/books?bibkeys=ISBN:0385472579&format=json&jscmd=data'
+    "#{Rails.application.secrets.open_library_api}books?bibkeys=ISBN:0385472579" \
+    '&format=json&jscmd=data'
   end
 end
