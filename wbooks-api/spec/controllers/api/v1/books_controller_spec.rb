87390b7e2e5c51fb6cffd39b439f322936e75f83
marcegl91 <marcegl91@users.noreply.github.com>
2019-04-11 15:33:27 -0300

    Add tests for books#search (#20)
    * Add tests for books#search
    
    * Refactor of book#search tests

--- wbooks-api/spec/controllers/api/v1/books_controller_spec.rb
+++ wbooks-api/spec/controllers/api/v1/books_controller_spec.rb
@@ -58,4 +58,47 @@ describe Api::V1::BooksController do
       end
     end
   end
+
+  describe 'GET #search' do
+    let!(:isbn) { '0385472579' }
+    let!(:invalid_isbn) { '13483783354345434' }
+
+    let!(:book) do
+      {
+        isbn: isbn,
+        title: Faker::Book.title,
+        subtitle: Faker::GreekPhilosophers.quote,
+        number_of_pages: Faker::Number.number(3),
+        authors: [
+          Faker::Book.author
+        ]
+      }
+    end
+
+    before do
+      open_library = class_double('OpenLibrary').as_stubbed_const
+      allow(open_library).to receive(:search).with(isbn).and_return(book)
+      allow(open_library).to receive(:search).with(invalid_isbn).and_return({})
+    end
+
+    context 'when searching a valid book' do
+      before do
+        get :search, params: { isbn: isbn }
+      end
+
+      it 'responses with the book' do
+        expect(book.stringify_keys).to eq response_body
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching an invalid book' do
+      subject(:http_request) { get :search, params: { isbn: invalid_isbn } }
+
+      it { is_expected.to have_http_status(:not_found) }
+    end
+  end
 end
